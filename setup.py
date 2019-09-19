c960a662c8e9aa5fd247994db0a80a92391b6f1b
Keven Carneiro <kevencarneiro@outlook.com>
2019-09-19 10:26:22 -0300

    Changing CI script

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
+import setuptools
 
 setup(name='cogroo_interface',
-      version='0.2',
+      version='0.3',
       description='Interface for accessing CoGrOO from Python scripts using py4j',
       author='Guilherme Passero',
       author_email='guilherme.passero0@gmail.com',
-      url='https://github.com/gpassero/cogroo4py',
+      url='https://github.com/kevencarneiro/cogroo4py',
+      include_package_data=True,
       py_modules=['cogroo_interface'],
       install_requires=['py4j', 'retry'],
       keywords=['cogroo'],
       classifiers=[
-          'Programming Language :: Python :: 3'
+          'Programming Language :: Python :: 3',
+          'Intended Audience :: Developers',
+          'Natural Language :: Portuguese (Brazilian)',
+          'Operating System :: OS Independent',
+          'Topic :: Text Processing'
       ])
