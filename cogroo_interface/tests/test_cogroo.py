99c839c5eb9a7c26700f3b7fb5dee68d8fd9b7d1
Keven Carneiro <kevencarneiro@outlook.com>
2021-11-18 20:08:51 -0300

    Implementing Github Actions

--- cogroo_interface/tests/test_cogroo.py
+++ cogroo_interface/tests/test_cogroo.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
+import unittest
 from cogroo_interface import Cogroo
 
 
-def test_lemmatize():
-    cogroo = Cogroo.Instance()
-    phrase_to_lemmatize = 'o entendimento das metas propostas oferece uma interessante oportunidade para verificação ' \
-                          'do impacto na agilidade decisória '
-    expected_result = 'o entender de o meta propor oferecer um interessante oportunidade para verificação de o ' \
-                      'impacto em o agilidade decisório'
-    assert expected_result == cogroo.lemmatize(phrase_to_lemmatize)
+class TestCogroo(unittest.TestCase):
+    def test_lemmatize(self):
+        cogroo = Cogroo()
+        phrase_to_lemmatize = 'o entendimento das metas propostas oferece uma interessante oportunidade para verificação ' \
+                            'do impacto na agilidade decisória '
+        expected_result = 'o entender de o meta propor oferecer um interessante oportunidade para verificação de o ' \
+                        'impacto em o agilidade decisório'
+        self.assertEqual(expected_result, cogroo.lemmatize(phrase_to_lemmatize))
+
+    def test_mistakes():
+        cogroo = Cogroo()
+        doc = cogroo.grammar_check('Elas são bonita')
+        self.assertTrue(doc.mistakes)
